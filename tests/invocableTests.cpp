e1e559e33a1ecd55cd75a8bfad2c0d704565498a
jstar920 <jianxing.jiang@qq.com>
2023-07-19 11:57:11 +0800

    add tests

--- tests/invocableTests.cpp
+++ tests/invocableTests.cpp
@@ -6,6 +6,8 @@ using namespace cpputils;
 TEST_CASE("invocable", "invocable")
 {
     auto f1 = [](){};
-    bool ok = is_invocable<std::decltype(f1)>::value;
+    bool ok = is_invocable<decltype(std::function<void()>(f1))>::value;
+    ASSURE_TRUE(ok);
+    ok = is_invocable<decltype(f1)>::value;
     ASSURE_TRUE(ok);
 }
