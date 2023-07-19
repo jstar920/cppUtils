8e5093d9ed83358f2110dce6d7b6625c0837b07e
jstar920 <jianxing.jiang@qq.com>
2023-07-19 19:17:04 +0800

    add invocable_r

--- src/type_traits/invocable.hpp
+++ src/type_traits/invocable.hpp
@@ -5,7 +5,10 @@
 namespace cpputils
 {
     template<typename F, typename... Args>
-    //struct is_invocable :  std::is_constructible<std::function<void(Args...)>, std::reference_wrapper<typename std::remove_reference<F>::type >>
     struct is_invocable :  std::is_constructible<std::function<void(Args...)>, F>
+    {};
+
+    template<typename F, typename R, typename... Args>
+    struct is_invocable_r :  std::is_constructible<std::function<R(Args...)>, F>
     {};
 }
